feef2a470f01a32511665d9ed1ad23f978e299ea
Çağdaş <cagdasfats@gmail.com>
2024-08-22 17:32:06 +0300

    side menu eklendi, sign up işlemleri yapıldı, validasyon mesajları yapılandırldı

--- src/pages/AddProductPage.tsx
+++ src/pages/AddProductPage.tsx
@@ -27,46 +27,49 @@ const AddProductPage: React.FC = () => {
         alert("ürün başarıyla eklendi");
       }
     ).catch(
-      error => {
-        alert("ürün eklenemedi");
+      err => {
+        if (err.response?.data?.errors) {
+          const errorMessages = Object.values(err.response.data.errors).flat();
+          const combinedErrors = errorMessages.join('\n');
+          alert(combinedErrors);
+        } else {
+          alert(err.response?.data || 'An error occurred');
+        }
       }
     );
   }
 
   return (
-    <div className="add-product-page-wrapper">
-      <div className="add-product-container">
-        <h2>Add New Product</h2>
-        <div className="form-row">
-          <div className="form-group">
-            <label>Product Name</label>
-            <input type="text" onChange={(e) => { setProductName(e.target.value) }} />
-          </div>
+    <div className="add-product-container">
+      <h2>Add New Product</h2>
+      <div className="form-row">
+        <div className="form-group">
+          <label>Product Name</label>
+          <input type="text" onChange={(e) => { setProductName(e.target.value) }} />
         </div>
-        <div className="form-row">
-          <div className="form-group">
-            <label>Product Category</label>
-            <input type="text" onChange={(e) => { setCategory(e.target.value) }} />
-          </div>
-          <div className="form-group">
-            <label>Price</label>
-            <input type="number" onChange={(e) => { setPrice(parseFloat(e.target.value)) }} />
-          </div>
+      </div>
+      <div className="form-row">
+        <div className="form-group">
+          <label>Product Category</label>
+          <input type="text" onChange={(e) => { setCategory(e.target.value) }} />
         </div>
         <div className="form-group">
-          <label>Product Description</label>
-          <textarea rows={4} onChange={(e) => { setDescription(e.target.value) }} />
+          <label>Price</label>
+          <input type="number" onChange={(e) => { setPrice(parseFloat(e.target.value)) }} />
         </div>
-        <div className="form-row">
-          <div className="form-group">
-            <label>Image URL</label>
-            <input type="text" onChange={(e) => { setImageUrl(e.target.value) }} />
-          </div>
-        </div>
-        <button className="submit-btn" onClick={()=>handleSave()}>Add Product</button>
       </div>
+      <div className="form-group">
+        <label>Product Description</label>
+        <textarea rows={4} onChange={(e) => { setDescription(e.target.value) }} />
+      </div>
+      <div className="form-row">
+        <div className="form-group">
+          <label>Image URL</label>
+          <input type="text" onChange={(e) => { setImageUrl(e.target.value) }} />
+        </div>
+      </div>
+      <button className="submit-btn" onClick={() => handleSave()}>Add Product</button>
     </div>
-
   );
 };
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,20 +3,26 @@ import "../styles/HomePage.css";
 import SearchIcon from '@mui/icons-material/Search';
 import PersonIcon from '@mui/icons-material/Person';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import AddIcon from '@mui/icons-material/Add';
+import MenuIcon from '@mui/icons-material/Menu';
 import ProductList from '../components/ProductList';
 import axios from 'axios';
 import { Product } from '../types';
-import { Route, Routes, useNavigate } from 'react-router-dom';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 import { useUser } from '../contexts/UserContext';
 import AddProductPage from './AddProductPage';
+import { Drawer, List, ListItem, ListItemText } from '@mui/material';
 
 const HomePage: React.FC = () => {
     const { setUser } = useUser();
     const navigate = useNavigate();
     const [products, setProducts] = useState<Product[]>([]);
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
+    const [open, setOpen] = useState(false);
     axios.defaults.withCredentials = true;
+
+    const toggleDrawer = () => {
+        setOpen(!open);
+    };
 
     useEffect(() => {
         axios.get("http://localhost:5212/api/Login/usercheck").then(
@@ -43,10 +49,31 @@ const HomePage: React.FC = () => {
         );
     }, []);
 
+    const handleLogout = () => {
+        axios.post("http://localhost:5212/api/Login/logout").then(
+            response => {
+                navigate("/");
+            }
+        );
+    }
+
     return (
         <div className="home-page-wrapper">
             <nav className="app-bar">
-                <span className="home-link" onClick={() => { navigate("/") }}>Hepsişurda</span>
+                <Drawer variant="temporary" open={open} onClose={toggleDrawer}>
+                    <List>
+                        <ListItem button component={Link} to='add-product' onClick={toggleDrawer}>
+                            <ListItemText style={{ padding: "5px 30px 5px 30px" }} primary={"Ürün Ekle"} />
+                        </ListItem>
+                    </List>
+                </Drawer>
+                <div className="home-and-menu">
+                    <div className="side-menu-button" onClick={toggleDrawer} >
+                        <MenuIcon className="side-menu-icon" />
+                        <MenuIcon className="side-menu-icon" id="side-menu-icon-shadow" />
+                    </div>
+                    <span className="home-link" onClick={() => { navigate("/") }}>Hepsişurada</span>
+                </div>
                 <div className="search-box">
                     <input type="text" placeholder="Ara" />
                     <SearchIcon className='search-icon'></SearchIcon>
@@ -56,9 +83,6 @@ const HomePage: React.FC = () => {
                     <button className="before-login-button" id='sign-up-button' onClick={() => navigate("/sign-up")}>Sign Up</button>
                 </div>
                 <div style={{ display: isLoggedIn ? "flex" : "none" }} className="after-login-buttons">
-                    <button className="circle-button" onClick={() => { navigate("add-product") }}>
-                        <AddIcon className="person-icon" />
-                    </button>
                     <div className="menu-button">
                         <button className="circle-button">
                             <ShoppingCartIcon className="shoppingCartIcon" />
@@ -68,6 +92,12 @@ const HomePage: React.FC = () => {
                         <button className="circle-button">
                             <PersonIcon className="person-icon" />
                         </button>
+                        <div className="dropdown-wrapper">
+                            <div className="account-dropdown-menu">
+                                <a>Profile</a>
+                                <a href="/" onClick={() => handleLogout()}>Logout</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </nav>
